2b4313294a2123f62f1e10a278baa45fbf684669
Szymon Kolber <kolberszymon@gmail.com>
2022-08-29 15:48:15 +0200

    Added wallet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,69 @@
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import '../assets/styles/index.css'
+import Head from "next/head";
+import { AppProps } from "next/app";
+import "../assets/styles/index.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  connectorsForWallets,
+  wallet,
+  lightTheme,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.hardhat],
+  [publicProvider()]
+);
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      wallet.rainbow({ chains }),
+      wallet.walletConnect({ chains }),
+      wallet.coinbase({ appName: "App", chains }),
+      wallet.metaMask({ chains }),
+      wallet.trust({ chains }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Ready4Player</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          theme={darkTheme({
+            accentColor: "#86F4FC",
+            borderRadius: "small",
+            accentColorForeground: "black",
+          })}
+        >
+          <Head>
+            <title>Ready4Player</title>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
